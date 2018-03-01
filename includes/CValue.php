9a384ebffbdfffbd6c8090e5b20d2b3bf5617992
Bradford Condon <bradford.condon@gmail.com>
2018-03-01 14:06:53 -0500

    init cvalue class

---
+++ includes/CValue.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace tripal_curator;
+
+
+/*
+ * This class is similar to Chado_property, but is for **CVALUES** instead of **type_ids.
+ *
+ *
+ */
+class CValue extends Chado_property {
+
+
+
+}
